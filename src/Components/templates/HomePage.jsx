da8efd1b2b0034680c8050c0747572694f476a5a
Yasaman Abedi <Yasaman471@gmail.com>
2025-12-13 10:47:51 +0330

    feat: Introduce CoinGecko service layer and render fetched data in TableCoins component

--- src/Components/templates/HomePage.jsx
+++ src/Components/templates/HomePage.jsx
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react"
 import TableCoins from "../modules/TableCoins"
+import { getCoinList } from "../../Services/CryptoApi"
 
 function HomePage() {
 
     const [coins,setCoins] = useState([])
-    
+
     useEffect(()=>{
-        const fetchData = async () => {
+        const getData = async () => {
             try {
-                const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en`)
+                const res = await fetch(getCoinList())
                 const json = await res.json()
-                    setCoins(json)
+                setCoins(json)
                 } catch (error) {
                 console.log(error.name)
                 }
         }
-        fetchData()
+        getData()
     },[])
   return (
     <div>
